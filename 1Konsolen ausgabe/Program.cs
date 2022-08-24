291c8d4e9d7684b365c0e7df346ec68f7c482ee8
Luca Heiler <luca.h41@gso.schule.koeln>
2022-08-24 12:23:16 +0200

    Projektdateien hinzufügen.

---
+++ 1Konsolen ausgabe/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+{
+    Console.WriteLine(" .-.");
+    Console.WriteLine("(o o)");
+    Console.WriteLine("");
+    Console.WriteLine();
+    Console.WriteLine();
+}   
+ 
