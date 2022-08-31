2b37bd49ec03346ef0fd301548404833349fe7b0
Luca Heiler <luca.h41@gso.schule.koeln>
2022-08-31 12:41:31 +0200

    aufgabe1 eingefügt

---
+++ 2 Datentyp/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(short.MaxValue);
+Console.WriteLine(short.MinValue);
+
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(float.MinValue);
+
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+
+
+
+string Vorname = "luca";
+short alter = 18;
+bool pro = true;
+
+Console.WriteLine(Vorname);
+Console.WriteLine(alter);
+Console.WriteLine(pro);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
