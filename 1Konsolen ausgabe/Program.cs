ac64050b005fa675838db06f2585bfb6383f3ea6
Luca Heiler <luca.h41@gso.schule.koeln>
2022-08-31 10:09:49 +0200

    Aufgabe 1,2,3

--- 1Konsolen ausgabe/Program.cs
+++ 1Konsolen ausgabe/Program.cs
@@ -3,8 +3,55 @@ Console.WriteLine("Hello, World!");
 {
     Console.WriteLine(" .-.");
     Console.WriteLine("(o o)");
-    Console.WriteLine("");
-    Console.WriteLine();
-    Console.WriteLine();
-}   
- 
+    Console.WriteLine("| O |");
+    Console.WriteLine("|   |");
+    Console.WriteLine("'~~~'");
+
+    {
+        Console.WriteLine("Aufgabe 2");
+
+
+
+
+        string Vorname = "Luca";
+        string Nachname = "Heiler";
+        int Alter = 17;
+
+        Console.WriteLine(Vorname);
+        Console.WriteLine(Nachname);
+        Console.WriteLine(Alter);
+
+    }
+
+    {
+        Console.WriteLine("Aufgabe 3");
+
+        Console.WriteLine("Bitte Vorname eingeben:");
+        string Vorname1 = Console.ReadLine();
+        Console.WriteLine("Bitte Klasse eingeben:");
+        string Klasse = Console.ReadLine();
+        Console.WriteLine("Bitte Alter eingeben:");
+        string Alter1 = Console.ReadLine();
+
+        Console.WriteLine("{0} ist {1} Jahre alt und besucht die {2} des Georg-Simon-Ohm Berufskollegs.",Vorname1,Alter1,Klasse);
+
+    }
+
+
+    {
+        
+
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+} 
